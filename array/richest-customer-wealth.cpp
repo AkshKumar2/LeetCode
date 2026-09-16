dc571e6d39da50b70ffdd25db4e0e4c3cebeab42
Aksh Kumar <akshkumarnew@gmail.com>
2026-09-16 10:54:18 +0530

    LC-richest-customer-wealth [Runtime: N/A; Memory: N/A]

---
+++ array/richest-customer-wealth.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max=0;
+        int s=0;
+        for(int i=0;i<accounts.size();i++){
+            s=0;
+            for(int j=0;j<accounts[i].size();j++){
+                s+=accounts[i][j];
+            }if(s>max)max=s;
+        }return max;
+    }
+};

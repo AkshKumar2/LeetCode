78918d8b90b1470742d60a27be6d20aad600969d
Aksh Kumar <akshkumarnew@gmail.com>
2026-09-18 20:04:19 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ binary-search/sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1,r=x,mid;
+        int s;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            s=mid*mid;
+            if(s==x)return mid;
+            else if(s>x)r=mid-1;
+            else l=mid+1;
+        }return r;
+    }
+};
